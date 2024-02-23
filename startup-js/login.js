90fa92400b473da6727ee57e3bd89f887cb794b8
Ryan Jensen <ryan.jensen2001@gmail.com>
2024-02-22 22:08:50 -0700

    now stores username and password on login

--- startup-js/login.js
+++ startup-js/login.js
@@ -1,6 +1,6 @@
 function login() {
-    const nameEl = document.querySelector("#loginUsername");
-    const pwdEl = document.querySelector("#loginPwd");
+    const nameEl = document.querySelector("#username");
+    const pwdEl = document.querySelector("#password");
     localStorage.setItem("userName", nameEl.value);
     localStorage.setItem("password", pwdEl.value);
     window.location.href = "play.html";
