3cb4e659c1141845b217088aa230275d34e3ac6d
Ryan Jensen <ryan.jensen2001@gmail.com>
2024-02-22 22:04:33 -0700

    modified login logic from simon to work on startup

--- startup-js/login.js
+++ startup-js/login.js
@@ -1,5 +1,7 @@
 function login() {
-    const nameEl = document.querySelector("#name");
+    const nameEl = document.querySelector("#loginUsername");
+    const pwdEl = document.querySelector("#loginPwd");
     localStorage.setItem("userName", nameEl.value);
+    localStorage.setItem("password", pwdEl.value);
     window.location.href = "play.html";
   }
