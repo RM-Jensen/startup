a8095c6fedfcc6f069c65cc18c34817589e0744c
Ryan Jensen <ryan.jensen2001@gmail.com>
2024-03-07 08:58:34 -0700

    index.js functions at simon level

--- startup-service/index.js
+++ startup-service/index.js
@@ -14,3 +14,23 @@ app.use(express.static('public'));
 const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 console.log("reached end of index.js\n");
+
+// GetScores
+apiRouter.get('/scores', (_req, res) => {
+  res.send(scores);
+});
+
+// SubmitScore
+apiRouter.post('/score', (req, res) => {
+  scores = updateScores(req.body, scores);
+  res.send(scores);
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
