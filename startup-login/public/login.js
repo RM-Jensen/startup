b37019b517df867b9e30c416db393601d351b115
Ryan Jensen <ryan.jensen2001@gmail.com>
2024-03-28 13:55:02 -0600

    bug fixed

--- startup-login/public/login.js
+++ startup-login/public/login.js
@@ -1,23 +1,24 @@
 
-function login() {
+async function login() {
   const userName = document.querySelector("#username").value;
   const password = document.querySelector("#password").value;
   localStorage.setItem("userName", userName);
-  loginOrCreate(userName, password, `/api/auth/create`);
+  await loginOrCreate(userName, password, `/api/auth/login`);
   window.location.href = "play.html";
 }
 
-function register() {
+async function register() {
   const userName = document.querySelector("#newUsername").value;
   const password = document.querySelector("#newPassword").value;
   localStorage.setItem("userName", userName);
-  loginOrCreate(userName, password, `/api/auth/create`);
+  await loginOrCreate(userName, password, `/api/auth/create`);
   window.location.href = "play.html";
 }
 
 async function loginOrCreate(userName, password, endpoint) {
   const response = await fetch(endpoint, {
     method: 'post',
+    credentials: 'include', // Don't forget to specify this if you need cookies
     body: JSON.stringify({ userName: userName, password: password }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
@@ -26,7 +27,7 @@ async function loginOrCreate(userName, password, endpoint) {
 
   if (response.ok) {
     localStorage.setItem('userName', userName);
-    window.location.href = 'play.html';
+//    window.location.href = 'play.html';
   } else {
     const body = await response.json();
     const modalEl = document.querySelector('#msgModal');
