f825e51e033eb0ed33d576d9a1ebb72c9f44e265
Ryan Jensen <ryan.jensen2001@gmail.com>
2024-04-17 11:31:29 -0600

    root page done, ready to add router

--- src/app.jsx
+++ src/app.jsx
@@ -5,26 +5,34 @@ import './app.css';
 export default function App() {
   return   (
 <div className='body bg-dark text-light'>
-  <header>
-      <h1>21 To Go</h1>
-      <nav>
-          <menu>
-          <li><a href="index.html">Login</a></li>
-          <li><a href="play.html">Play</a></li>
-          <li><a href="scores.html">Scores</a></li>
+  <header className='container-fluid'>
+      <nav className='navbar fixed-top navbar-dark'>
+            <h1>21 To Go</h1>
+          <menu className='navbar-nav'>
+          <li className='nav-item'>
+            <a className='nav-link' href="index.html">Login</a>
+            </li>
+          <li className='nav-item'>
+            <a className='nav-link' href="play.html">Play</a>
+            </li>
+          <li className='nav-item'>
+            <a className='nav-link' href="scores.html">Scores</a>
+            </li>
           </menu>
       </nav>
-      <span id="nameDisplay">Super cool user</span>
+      
   </header>
 
-  <main>
-  <div className='body bg-dark text-light'>App will display here</div>;
-  </main>
+  <main>App components go here</main>
+
+
   <footer>
-  <span class="text-reset">Ryan Jensen</span>
-  <a href="https://github.com/RM-Jensen/startup">GitHub</a>
+    <div className='container-fluid'>
+        <span className="text-reset">Ryan Jensen</span>
+        <a className='text-reset' href="https://github.com/RM-Jensen/startup">GitHub</a>
+    </div>
   </footer>
-  <script src="play.js"></script>
+
 </div>)
   
   
---
+++ src/play/play.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Login() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>game displayed here</div>
+    </main>
+  );
+}
