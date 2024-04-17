8713a1fe3fdec32dd2eb602a62c7fc1f10982246
Ryan Jensen <ryan.jensen2001@gmail.com>
2024-04-16 20:18:04 -0600

    header and footer

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return   (
+<div className='body bg-dark text-light'>
+  <header>
+      <h1>21 To Go</h1>
+      <nav>
+          <menu>
+          <li><a href="index.html">Login</a></li>
+          <li><a href="play.html">Play</a></li>
+          <li><a href="scores.html">Scores</a></li>
+          </menu>
+      </nav>
+      <span id="nameDisplay">Super cool user</span>
+  </header>
+
+  <main>
+
+  </main>
+  <footer>
+  <span class="text-reset">Ryan Jensen</span>
+  <a href="https://github.com/RM-Jensen/startup">GitHub</a>
+  </footer>
+  <script src="play.js"></script>
+</div>)
+  
+  
+  <div className='body bg-dark text-light'>App will display here</div>;
 }
